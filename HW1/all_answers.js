2ef3972668fdc6768ecdfaf7ba0436170790cfcc
Alesia Hancharyk <goncharik.alesya@gmail.com>
2025-03-25 13:16:15 +0100

    moved all solutions to one js and one html file

---
+++ HW1/all_answers.js
@@ -0,0 +1,55 @@
+// Answer for task 2
+console.log("Hancharyk")
+
+// Answer for task 4
+const myObject = {
+    String: "Hello, world!",
+    Number: 42,
+    Boolean: true,
+    Undefined: undefined,
+    Null: null
+  };
+
+  // Answer for task 5 
+let isAdult = confirm("Have you reached the age of majority?");
+console.log("Is the user an adult?", isAdult);
+
+// Answer for task 3
+let num1 = 1;
+console.log("Variable 1 is ", num1)
+let num2 = 2;
+console.log("Variable 2 is ", num2)
+num1 = num2;
+console.log("Variable 1 after copying is ", num1)
+
+
+// Answer for task 6
+let group = "jsRetraining";
+let birthYear = 1991;
+let maritalStatus = true;
+
+let empty = null;
+let nothing;
+
+console.log("Type of birthYear var is ", typeof birthYear);
+console.log("Type of maritalStatus var is ", typeof maritalStatus);
+console.log("Type of group var is ", typeof group);
+console.log("Type of surname var is ", typeof surname);
+console.log("Type of name var is ", typeof name);
+
+console.log("Type of name var is ", typeof empty);
+console.log("Type of name var is ", typeof nothing);
+
+// Answer for task 7
+let login = prompt("What is your login?", "Enter login here");
+let email = prompt("What is your email?", "Enter email here");
+let password = prompt("What is your password?", "Enter password here");
+alert(`Dear User ${login}, your email is ${email}, your password is ${password}.`)
+
+// Answer for task 8
+let secondsInMinute = 60;
+let secondsInHour = 60 * secondsInMinute;
+let secondsInDay = 24 * secondsInHour;
+let secondsInMonth = 30 * secondsInDay;
+
+alert(`Amount of seconds in an hour is ${secondsInHour}, in a day is ${secondsInDay}, in a month is ${secondsInMonth}.`)
