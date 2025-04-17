081b93fadf0f0e0645df4b266f8cc1885868f154
Alesia Hancharyk <goncharik.alesya@gmail.com>
2025-04-17 20:29:59 +0200

    added task2

--- HW5/hw5.js
+++ HW5/hw5.js
@@ -15,3 +15,40 @@ console.log(propsCount(mentor));
 
 // Task 2 
 
+let testObject = {
+    fieldOne: "Value One",
+    fieldTwo: "Value Two",
+    fieldThree: "Value Three",
+    fieldFour: "Value Four",
+    fieldFive: "Value Five"
+}
+
+function showProps(obj) {
+    let result = Object.keys(obj);
+    return result;
+}
+
+console.log(showProps(testObject));
+
+//Task 3 
+
+// 3. Create the Person class , in which the constructor accepts the name and surname parameters 
+// and also contains the showFullName () method, which displays the person's first and last name.
+
+// Student class is inherited from the Person class , 
+// the constructor of which, in addition to name and surname , 
+// accepts the year parameter (year of admission to the university).
+// In the Student class it is necessary to redefine the method showFullName ( middleName ) 
+// so that not only the name, surname, but also the patronymic ( middleName ) of the student is displayed.
+// Also in the Student class it is necessary to implement the showCourse () method , 
+// which will display the current course of the student (from 1 to 6). 
+// The value of the course will be determined as the difference between the current year (determine independently) 
+// and the year of admission to the university .
+
+// Example result :
+// const stud1 = new Student("Petro", " Petrenko ", 2015);
+// console.log(stud1.showFullName(" Petrovych ")); // Petrenko Petro Petrovych
+// console.log("Current course: " + stud1.showCourse()); //Current course: 6
+
+
+
