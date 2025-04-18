9d9ce1792468a8dbef2045a2629b95db169304bc
Alesia Hancharyk <goncharik.alesya@gmail.com>
2025-04-18 09:55:20 +0200

    added task 3

--- HW5/hw5.js
+++ HW5/hw5.js
@@ -31,24 +31,32 @@ function showProps(obj) {
 console.log(showProps(testObject));
 
 //Task 3 
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    showFullName() {
+        alert(this.firstName, this.lastName);
+    }
 
-// 3. Create the Person class , in which the constructor accepts the name and surname parameters 
-// and also contains the showFullName () method, which displays the person's first and last name.
+}
+class Student extends Person {
+    constructor(firstName, lastName, year) {
+        super(firstName, lastName);
+        this.year = parseInt(year);
+    }
+    showFullName(middleName) {
+        this.middleName = middleName;
+        alert(this.firstName + " " + this.middleName + " " + this.lastName);
+    }
+    showCourse() {
+        let currentYear = new Date().getFullYear();
+        return currentYear - this.year;
+    }
+}
 
-// Student class is inherited from the Person class , 
-// the constructor of which, in addition to name and surname , 
-// accepts the year parameter (year of admission to the university).
-// In the Student class it is necessary to redefine the method showFullName ( middleName ) 
-// so that not only the name, surname, but also the patronymic ( middleName ) of the student is displayed.
-// Also in the Student class it is necessary to implement the showCourse () method , 
-// which will display the current course of the student (from 1 to 6). 
-// The value of the course will be determined as the difference between the current year (determine independently) 
-// and the year of admission to the university .
-
-// Example result :
-// const stud1 = new Student("Petro", " Petrenko ", 2015);
-// console.log(stud1.showFullName(" Petrovych ")); // Petrenko Petro Petrovych
-// console.log("Current course: " + stud1.showCourse()); //Current course: 6
-
-
-
+// Added spaces in the function, as it seems more common + changed order in giving first, middle, last 
+const stud1 = new Student("Petro", "Petrenko", 2024);
+console.log(stud1.showFullName("Petrovych"));
+console.log("Current course: " + stud1.showCourse()); 
